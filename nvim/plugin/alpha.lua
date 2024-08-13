68619da991023f3d6e85c35aabfde9b472ae8053
Eddio0141 <eddio0141@gmail.com>
2024-08-13 02:26:43 +0100

    made it meow

--- nvim/plugin/alpha.lua
+++ nvim/plugin/alpha.lua
@@ -1,1 +1,23 @@
-require("alpha").setup(require("alpha.themes.dashboard").config)
+local dashboard = require('alpha.themes.dashboard')
+dashboard.section.header.val = {
+  [[                                                                 $$\               ]],
+  [[                                                                 \__|              ]],
+  [[$$$$$$\$$$$\   $$$$$$\   $$$$$$\  $$\  $$\  $$\       $$\    $$\ $$\ $$$$$$\$$$$\  ]],
+  [[$$  _$$  _$$\ $$  __$$\ $$  __$$\ $$ | $$ | $$ |      \$$\  $$  |$$ |$$  _$$  _$$\ ]],
+  [[$$ / $$ / $$ |$$$$$$$$ |$$ /  $$ |$$ | $$ | $$ |       \$$\$$  / $$ |$$ / $$ / $$ |]],
+  [[$$ | $$ | $$ |$$   ____|$$ |  $$ |$$ | $$ | $$ |        \$$$  /  $$ |$$ | $$ | $$ |]],
+  [[$$ | $$ | $$ |\$$$$$$$\ \$$$$$$  |\$$$$$\$$$$  |         \$  /   $$ |$$ | $$ | $$ |]],
+  [[\__| \__| \__| \_______| \______/  \_____\____/           \_/    \__|\__| \__| \__|]],
+}
+-- dashboard.section.buttons.val = {
+--   dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+--   dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+-- }
+-- local handle = io.popen('fortune')
+-- local fortune = handle:read("*a")
+-- handle:close()
+-- dashboard.section.footer.val = fortune
+
+-- dashboard.config.opts.noautocmd = true
+
+require("alpha").setup(dashboard.config)
