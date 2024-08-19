303e2c0a8ad1287d99974c9c35ea9a9f5d2a2dcc
Eddio0141 <eddio0141@gmail.com>
2024-08-19 22:22:38 +0100

    highlight on yank is more visible

--- nvim/plugin/autocommands.lua
+++ nvim/plugin/autocommands.lua
@@ -182,6 +182,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Hightlight selection on yank',
   pattern = '*',
   callback = function()
-    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 250 }
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
   end,
 })
