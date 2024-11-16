3edcbf760e4a664bf065db620b447e5bd3922395
Eddio0141 <eddio0141@gmail.com>
2024-11-16 18:42:50 +0000

    fixed probably

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -33,17 +33,15 @@ return {
         args = { "--port", "${port}" }
       }
     }
-    dap.configurations = {
-      cpp = {
-        name = "Launch (CodeLLDB)",
-        type = "codelldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false
-      }
+    dap.configurations.cpp = {
+      name = "Launch (CodeLLDB)",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false
     }
 
     dap.configurations.c = dap.configurations.cpp
