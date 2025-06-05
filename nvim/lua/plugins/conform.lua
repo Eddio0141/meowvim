ebe4bc62d80cd72b7469b5a0d4b03bd66061d5bc
edcope <ed.cope@codethink.co.uk>
2025-06-05 14:09:09 +0100

    fix ruff format stupidity

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,7 +6,7 @@ return {
     conform.setup({
       formatters_by_ft = {
         nix = { "nixfmt" },
-        python = { "ruff" },
+        python = { "ruff_format" },
       },
       format_on_save = {
         timeout_ms = 500,
