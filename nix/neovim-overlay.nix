cf5188a8691c2d9e9fbe7218cbd80280ba5688d4
edcope <ed.cope@codethink.co.uk>
2025-08-20 10:20:46 +0100

    disable aw from spamming on startup on no install

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -104,15 +104,15 @@ let
       # ^ bleeding-edge plugins from flake inputs
       rustaceanvim
       alpha-nvim
-      (pkgs.vimUtils.buildVimPlugin {
-        name = "activitywatch";
-        src = pkgs.fetchFromGitHub {
-          owner = "activitywatch";
-          repo = "aw-watcher-vim";
-          rev = "4ba86d05a940574000c33f280fd7f6eccc284331";
-          hash = "sha256-I7YYvQupeQxWr2HEpvba5n91+jYvJrcWZhQg+5rI908=";
-        };
-      })
+      # (pkgs.vimUtils.buildVimPlugin {
+      #   name = "activitywatch";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "activitywatch";
+      #     repo = "aw-watcher-vim";
+      #     rev = "4ba86d05a940574000c33f280fd7f6eccc284331";
+      #     hash = "sha256-I7YYvQupeQxWr2HEpvba5n91+jYvJrcWZhQg+5rI908=";
+      #   };
+      # })
       nvim-notify
       nvim-lspconfig
       rtp-nvim
