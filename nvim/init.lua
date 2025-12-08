eccb8a2de696b28342c864abd03a21f9ce1d59ee
Eddio0141 <eddio0141@gmail.com>
2025-12-08 11:35:31 +0000

    stop hidden messages

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,7 +34,7 @@ opt.nrformats = 'bin,hex' -- 'octal'
 opt.undofile = true
 opt.splitright = true
 opt.splitbelow = true
-opt.cmdheight = 0
+-- opt.cmdheight = 0
 
 opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 opt.colorcolumn = '100'
