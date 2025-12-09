2e456eda2db18c90680955714cc811ec04c38c25
Eddio0141 <eddio0141@gmail.com>
2025-12-09 10:56:33 +0000

    leap: stop conflicting with surround-nvim

--- nvim/plugin/leap.lua
+++ nvim/plugin/leap.lua
@@ -1,4 +1,21 @@
 local keymap = vim.keymap
 
-keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+keymap.set({ 'n', 'x' }, 's', '<Plug>(leap)')
+keymap.set({ 'o' }, 'x', '<Plug>(leap)')
 keymap.set('n', 'S', '<Plug>(leap-from-window)')
+
+local leap = require("leap")
+local leap_user = require("leap.user")
+
+leap.opts.preview = function(ch0, ch1, ch2)
+  return not (
+    ch1:match('%s')
+    or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
+  )
+end
+
+leap.opts.equivalence_classes = {
+  ' \t\r\n', '([{', ')]}', '\'"`'
+}
+
+leap_user.set_repeat_keys('<enter>', '<backspace>')
