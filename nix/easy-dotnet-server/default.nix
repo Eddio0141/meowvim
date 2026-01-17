d4bf029766e80e15f86e482ebc599f75e0b6ff95
Eddio0141 <eddio0141@gmail.com>
2026-01-17 00:10:03 +0000

    updated meowvim

--- nix/easy-dotnet-server/default.nix
+++ nix/easy-dotnet-server/default.nix
@@ -5,8 +5,8 @@
 buildDotnetGlobalTool {
   pname = "dotnet-easydotnet";
   nugetName = "EasyDotnet";
-  version = "2.3.58";
-  nugetHash = "sha256-pjGlLNdKDVRqZX6tpHroIXgHQgS8nCUgjRMp9li0BvA=";
+  version = "2.3.59";
+  nugetHash = "sha256-12KlUR5d8aQnro2HGzhtoqdAz7CIMZuwqvrN4sENrFs=";
   postFixup = ''
     cd "$out/bin"
     ln -s dotnet-easydotnet easydotnet
