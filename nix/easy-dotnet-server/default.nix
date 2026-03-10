e700dce91947d9cdf47e796ee789425476377e95
Eddio0141 <eddio0141@gmail.com>
2026-03-10 21:48:02 +0000

    updated easy-dotnet

--- nix/easy-dotnet-server/default.nix
+++ nix/easy-dotnet-server/default.nix
@@ -5,8 +5,8 @@
 buildDotnetGlobalTool {
   pname = "dotnet-easydotnet";
   nugetName = "EasyDotnet";
-  version = "2.9.9";
-  nugetHash = "sha256-9tt5j4BdmOr8so78ntLjj2Xh7c5mCTPyCPfSzLqg2pk=";
+  version = "3.0.0";
+  nugetHash = "sha256-olaARuN/RErqyZAIiDeHZlYAxlNM/Qfn6wk73nfRBjM=";
   postFixup = ''
     cd "$out/bin"
     ln -s dotnet-easydotnet easydotnet
