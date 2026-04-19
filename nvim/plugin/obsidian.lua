35b45a6fdd139c8a6595688ac1983d264aed2bff
Eddio0141 <eddio0141@gmail.com>
2026-04-19 22:19:01 +0100

    fixed obsidian.nvim

--- nvim/plugin/obsidian.lua
+++ nvim/plugin/obsidian.lua
@@ -1,6 +1,7 @@
 require("obsidian").setup({
   legacy_commands = false,
   workspaces = {
-    { name = "work", path = "~/Documents/work-notes" }
+    { name = "work", path = "~/Documents/work-notes" },
+    { name = "home", path = "~/Documents/Obsidian" }
   }
 })
